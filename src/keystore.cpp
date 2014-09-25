0036dcff57fe9b700ceedb3d16c9862d373829c6
Cory Fields <cory-nospam-@coryfields.com>
2014-09-24 22:33:56 -0400

    script: remove all uses of CScript::GetID()

--- src/keystore.cpp
+++ src/keystore.cpp
@@ -39,7 +39,7 @@ bool CBasicKeyStore::AddCScript(const CScript& redeemScript)
         return error("CBasicKeyStore::AddCScript() : redeemScripts > %i bytes are invalid", MAX_SCRIPT_ELEMENT_SIZE);
 
     LOCK(cs_KeyStore);
-    mapScripts[redeemScript.GetID()] = redeemScript;
+    mapScripts[CScriptID(redeemScript)] = redeemScript;
     return true;
 }
 
