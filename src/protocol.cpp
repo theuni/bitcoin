6733b649ae02c7130269af386c48e4b211a0166c
Cory Fields <cory-nospam-@coryfields.com>
2015-01-23 23:28:22 -0500

    MOVEONLY: move CAddress to netbase.o for now
    CAddress is very tangled up, move it out of protocol and handle it later.

--- src/protocol.cpp
+++ src/protocol.cpp
@@ -73,26 +73,6 @@ bool CMessageHeader::IsValid() const
     return true;
 }
 
-
-
-CAddress::CAddress() : CService()
-{
-    Init();
-}
-
-CAddress::CAddress(CService ipIn, uint64_t nServicesIn) : CService(ipIn)
-{
-    Init();
-    nServices = nServicesIn;
-}
-
-void CAddress::Init()
-{
-    nServices = NODE_NETWORK;
-    nTime = 100000000;
-    nLastTry = 0;
-}
-
 CInv::CInv()
 {
     type = 0;
