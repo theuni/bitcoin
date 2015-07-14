a935f57920c2585649a320e921c9f622f99fc71b
Cory Fields <cory-nospam-@coryfields.com>
2015-07-14 04:01:52 -0400

    depends: make boost deterministic for osx

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -25,6 +25,7 @@ $(package)_cxxflags_linux=-fPIC
 endef
 
 define $(package)_preprocess_cmds
+  sed -i.old "s/-gdwarf-2//" tools/build/src/tools/darwin.jam && \
   echo "using $(boost_toolset_$(host_os)) : : $($(package)_cxx) : <cxxflags>\"$($(package)_cxxflags) $($(package)_cppflags)\" <linkflags>\"$($(package)_ldflags)\" <archiver>\"$(boost_archiver_$(host_os))\" <striper>\"$(host_STRIP)\"  <ranlib>\"$(host_RANLIB)\" <rc>\"$(host_WINDRES)\" : ;" > user-config.jam
 endef
 
