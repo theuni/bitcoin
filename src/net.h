8a9c143b20c349f717937cb03e334c45874efb54
Cory Fields <cory-nospam-@coryfields.com>
2016-04-18 21:33:54 -0400

    net: move messageHandlerCondition to CConnman

--- src/net.h
+++ src/net.h
@@ -226,6 +226,7 @@ private:
     std::vector<CNode*> vNodes;
     mutable CCriticalSection cs_vNodes;
     std::atomic<NodeId> nLastNodeId;
+    boost::condition_variable messageHandlerCondition;
 };
 extern std::unique_ptr<CConnman> g_connman;
 void MapPort(bool fUseUPnP);
@@ -547,7 +548,7 @@ public:
     }
 
     // requires LOCK(cs_vRecvMsg)
-    bool ReceiveMsgBytes(const char *pch, unsigned int nBytes);
+    bool ReceiveMsgBytes(const char *pch, unsigned int nBytes, bool& complete);
 
     // requires LOCK(cs_vRecvMsg)
     void SetRecvVersion(int nVersionIn)
