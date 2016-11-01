8423734bd9600e5d0866bf70412ea5afae651241
Cory Fields <cory-nospam-@coryfields.com>
2016-11-01 15:50:43 -0400

    uint: add user-defined literals for uint256 and uint160
    Now we can simply (and at build-time) construct uints.

--- src/uint256.h
+++ src/uint256.h
@@ -118,6 +118,7 @@ public:
     uint160() {}
     uint160(const base_blob<160>& b) : base_blob<160>(b) {}
     explicit uint160(const std::vector<unsigned char>& vch) : base_blob<160>(vch) {}
+    constexpr explicit uint160(std::array<uint8_t, 20> vch) : base_blob(vch){}
 };
 
 /** 256-bit opaque blob.
@@ -130,6 +131,7 @@ public:
     uint256() {}
     uint256(const base_blob<256>& b) : base_blob<256>(b) {}
     explicit uint256(const std::vector<unsigned char>& vch) : base_blob<256>(vch) {}
+    constexpr explicit uint256(std::array<uint8_t, 32> vch) : base_blob(vch){}
 
     /** A cheap hash function that just returns 64 bits from the result, it can be
      * used when the contents are considered uniformly random. It is not appropriate
@@ -141,6 +143,66 @@ public:
         return ReadLE64(data.data());
     }
 };
+
+template <size_t len>
+struct arraymaker
+{
+    static constexpr char ascii2int(const char in)
+    {
+        return in >= 48 && in <= 57  ? in - 48 :
+               in >= 65 && in <= 70  ? in - 55 :
+               in >= 97 && in <= 102 ? in - 87 : -1;
+    }
+
+    template <int n>
+    static constexpr uint8_t getn(const std::array<char, len * 2>& arr)
+    {
+        static_assert(n < len * 2, "index out of range");
+        return static_cast<uint8_t>(ascii2int((std::get<n>(arr))) << 4 | ascii2int((std::get<n+1>(arr))));
+    }
+
+    static constexpr std::array<uint8_t, len> parse(std::array<char, len * 2> arr);
+
+    template<char a, char b, char... c >
+    static constexpr std::array<uint8_t, len> make()
+    {
+        static_assert(a == '0',"hex literal must begin with 0x");
+        static_assert(b == 'x' || b == 'X',"hex literal must begin with 0x");
+        return parse({{c...}});
+    }
+};
+
+template<>
+constexpr std::array<uint8_t, 32> arraymaker<32>::parse(std::array<char, 32 * 2> arr)
+{
+    return  {{
+                getn<62>(arr), getn<60>(arr), getn<58>(arr), getn<56>(arr), getn<54>(arr), getn<52>(arr), getn<50>(arr), getn<48>(arr),
+                getn<46>(arr), getn<44>(arr), getn<42>(arr), getn<40>(arr), getn<38>(arr), getn<36>(arr), getn<34>(arr), getn<32>(arr),
+                getn<30>(arr), getn<28>(arr), getn<26>(arr), getn<24>(arr), getn<22>(arr), getn<20>(arr), getn<18>(arr), getn<16>(arr),
+                getn<14>(arr), getn<12>(arr), getn<10>(arr), getn< 8>(arr), getn< 6>(arr), getn< 4>(arr), getn< 2>(arr), getn< 0>(arr)
+            }};
+}
+
+template<>
+constexpr std::array<uint8_t, 20> arraymaker<20>::parse(std::array<char, 20 * 2> arr)
+{
+    return  {{
+                getn<18>(arr), getn<16>(arr), getn<14>(arr), getn<12>(arr), getn<10>(arr),
+                getn< 8>(arr), getn< 6>(arr), getn< 4>(arr), getn< 2>(arr), getn< 0>(arr)
+            }};
+}
+
+template< char... c >
+constexpr uint256 operator "" _u256 () {
+    static_assert(sizeof...(c) == 66, "bad length");
+    return uint256{arraymaker<32>::make<c...>()};
+}
+
+template< char... c >
+constexpr uint160 operator "" _u160 () {
+    static_assert(sizeof...(c) == 42, "bad length");
+    return uint160{arraymaker<20>::make<c...>()};
+}
 
 /* uint256 from const char *.
  * This is a separate function because the constructor uint256(const char*) can result
