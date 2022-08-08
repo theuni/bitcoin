25a2220246557fee1f4d91061b090980e1db50b5
Cory Fields <cory-nospam-@coryfields.com>
2022-08-08 21:34:30 +0000

    Bump bitcoin-tidy-experiments plugin
    It is now able to fully and correctly transform libbitcoin-kernel.

--- depends/packages/native_bitcoin_tidy.mk
+++ depends/packages/native_bitcoin_tidy.mk
@@ -1,8 +1,8 @@
 package=native_bitcoin_tidy
-$(package)_version=aa62f3e2279ef4b9cbba7a6e2dc6185fefb39a23
+$(package)_version=e9717e5120d5342faac005651d5a2cd8a7fa33fc
 $(package)_download_path=https://github.com/theuni/bitcoin-tidy-experiments/archive
 $(package)_file_name=$($(package)_version).tar.gz
-$(package)_sha256_hash=34a4eabfca8a02f47d61531b6262ade3d4df53e281c6c69c687f914eb978475d
+$(package)_sha256_hash=b2a9b530673420ad1341591d9d2197abda92441e2b27806c07251292f39053ed
 $(package)_dependencies=native_clang_headers
 define $(package)_config_cmds
   $($(package)_cmake)
