e131d8f1e33baa9a9499d2e1a4a99b171566c5a5
Cory Fields <cory-nospam-@coryfields.com>
2022-09-09 16:47:35 +0000

    Quick hack to move boost include path out of prefix

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -5,6 +5,6 @@ $(package)_file_name=boost_$(subst .,_,$($(package)_version)).tar.bz2
 $(package)_sha256_hash=fc9f85fc030e233142908241af7a846e60630aa7388de9a5fafb1f3a26840854
 
 define $(package)_stage_cmds
-  mkdir -p $($(package)_staging_prefix_dir)/include && \
-  cp -r boost $($(package)_staging_prefix_dir)/include
+  mkdir -p $($(package)_staging_prefix_dir)/boost/include && \
+  cp -r boost $($(package)_staging_prefix_dir)/boost/include
 endef
