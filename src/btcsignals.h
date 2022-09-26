990a627befd644ce53d0f77d4b4e68cbe53462cd
Cory Fields <cory-nospam-@coryfields.com>
2022-09-26 21:13:15 +0000

    signals: first whack at a compatible signals api
    only build, no work.

--- src/btcsignals.h
+++ src/btcsignals.h
@@ -7,7 +7,7 @@
 #define BITCOIN_BTCSIGNALS_H
 
 
-#if 1
+#if 0
 
 #include <boost/signals2/connection.hpp>
 #include <boost/signals2/optional_last_value.hpp>
@@ -17,15 +17,56 @@ namespace btcsignals = boost::signals2;
 
 #else
 
-template <typename T, typename U>
-struct signal
+#include <optional>
+
+namespace btcsignals
 {
-};
 
 struct connection
 {
+    void disconnect(){};
 };
 
+struct scoped_connection : public connection
+{
+    scoped_connection(connection&&){};
+};
+
+template<typename T>
+class optional_last_value {
+public:
+    using result_type = std::optional<T>;
+
+    template<typename InputIterator>
+    result_type operator()(InputIterator, InputIterator) const
+    {
+        return {};
+    }
+};
+
+template <typename Signature, typename Combiner = optional_last_value<bool>>
+struct signal
+{
+    template <typename... Args>
+    typename Combiner::result_type operator()(Args&&... args)
+    {
+        return {};
+    }
+
+    template <typename Callable, typename... Args>
+    connection connect(Callable, Args&&... args)
+    {
+        return {};
+    }
+
+    bool empty() const
+    {
+        return true;
+    }
+
+};
+
+} // namespace btcsignals
 #endif
 
 #endif // BITCOIN_BTCSIGNALS_H
