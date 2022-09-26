0c308c8801cd323db174342e9554820a8f4068ca
Cory Fields <cory-nospam-@coryfields.com>
2022-09-26 16:45:06 +0000

    depends: patch boost::signals2 to use std::mutex when available

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -3,6 +3,11 @@ $(package)_version=1.80.0
 $(package)_download_path=https://boostorg.jfrog.io/artifactory/main/release/$($(package)_version)/source/
 $(package)_file_name=boost_$(subst .,_,$($(package)_version)).tar.bz2
 $(package)_sha256_hash=1e19565d82e43bc59209a168f5ac899d3ba471d55c7610c677d4ccf2c9c500c0
+$(package)_patches=signals2-stdmutex.patch
+
+define $(package)_preprocess_cmds
+  patch -p1 < $($(package)_patch_dir)/signals2-stdmutex.patch
+endef
 
 define $(package)_stage_cmds
   mkdir -p $($(package)_staging_prefix_dir)/include && \
