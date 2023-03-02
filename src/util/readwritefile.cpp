cec8cb98e4f42455cb689e306883418d5e9f5857
Cory Fields <cory-nospam-@coryfields.com>
2023-03-02 20:30:17 +0000

    util: add missing include and fix function signature

--- src/util/readwritefile.cpp
+++ src/util/readwritefile.cpp
@@ -4,6 +4,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include <fs.h>
+#include <util/readwritefile.h>
 
 #include <algorithm>
 #include <cstdio>
@@ -11,7 +12,7 @@
 #include <string>
 #include <utility>
 
-std::pair<bool,std::string> ReadBinaryFile(const fs::path &filename, size_t maxsize=std::numeric_limits<size_t>::max())
+std::pair<bool,std::string> ReadBinaryFile(const fs::path &filename, size_t maxsize)
 {
     FILE *f = fsbridge::fopen(filename, "rb");
     if (f == nullptr)
