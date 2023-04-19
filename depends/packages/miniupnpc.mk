04c50345bda28b9e48364bbf6c9245d39dd0a4a6
Cory Fields <cory-nospam-@coryfields.com>
2023-04-19 16:49:04 -0400

    depends: switch miniupnpc to CMake

--- depends/packages/miniupnpc.mk
+++ depends/packages/miniupnpc.mk
@@ -4,14 +4,15 @@ $(package)_download_path=https://miniupnp.tuxfamily.org/files/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=888fb0976ba61518276fe1eda988589c700a3f2a69d71089260d75562afd3687
 $(package)_patches=dont_leak_info.patch respect_mingw_cflags.patch
+$(package)_dependencies=native_cmake
+$(package)_build_subdir=build
 
 # Next time this package is updated, ensure that _WIN32_WINNT is still properly set.
 # See discussion in https://github.com/bitcoin/bitcoin/pull/25964.
 define $(package)_set_vars
-$(package)_build_opts=CC="$($(package)_cc)"
-$(package)_build_opts_darwin=LIBTOOL="$($(package)_libtool)"
-$(package)_build_opts_mingw32=-f Makefile.mingw CFLAGS="$($(package)_cflags) -D_WIN32_WINNT=0x0601"
-$(package)_build_env+=CFLAGS="$($(package)_cflags) $($(package)_cppflags)" AR="$($(package)_ar)"
+$(package)_cmake_opts = -DUPNPC_BUILD_SAMPLE=OFF -DUPNPC_BUILD_SHARED=OFF
+$(package)_cmake_opts += -DUPNPC_BUILD_STATIC=ON -DUPNPC_BUILD_TESTS=OFF
+$(package)_cppflags_mingw32=-D_WIN32_WINNT=0x0601"
 endef
 
 define $(package)_preprocess_cmds
@@ -19,12 +20,14 @@ define $(package)_preprocess_cmds
   patch -p1 < $($(package)_patch_dir)/respect_mingw_cflags.patch
 endef
 
+define $(package)_config_cmds
+  $($(package)_cmake) -S .. -B .
+endef
+
 define $(package)_build_cmds
-	$(MAKE) libminiupnpc.a $($(package)_build_opts)
+	$(MAKE)
 endef
 
 define $(package)_stage_cmds
-	mkdir -p $($(package)_staging_prefix_dir)/include/miniupnpc $($(package)_staging_prefix_dir)/lib &&\
-	install *.h $($(package)_staging_prefix_dir)/include/miniupnpc &&\
-	install libminiupnpc.a $($(package)_staging_prefix_dir)/lib
+	$(MAKE) DESTDIR=$($(package)_staging_dir) install
 endef
