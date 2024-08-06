461820bff7fca9bb96b9752039bef3e7046e5df3
Cory Fields <cory-nospam-@coryfields.com>
2024-08-06 15:21:05 +0000

    mempool: define txiter using tag rather than index order

--- src/txmempool.h
+++ src/txmempool.h
@@ -392,7 +392,7 @@ public:
     mutable RecursiveMutex cs;
     indexed_transaction_set mapTx GUARDED_BY(cs);
 
-    using txiter = indexed_transaction_set::nth_index<0>::type::const_iterator;
+    using txiter = indexed_transaction_set::index<index_by_txid>::type::const_iterator;
     std::vector<CTransactionRef> txns_randomized GUARDED_BY(cs); //!< All transactions in mapTx, in random order
 
     typedef std::set<txiter, CompareIteratorByHash> setEntries;
