f3c7726009dab2567ce6fda9387b03249af21092
Cory Fields <cory-nospam-@coryfields.com>
2025-01-13 22:31:01 +0000

    depends: add FOO_CONFIG flag for config-specific flags
    For example: CXXFLAGS_CONFIG="-O3" would override host_release_CXXFLAGS while
    leaving host_CXXFLAGS in place.

--- depends/hosts/default.mk
+++ depends/hosts/default.mk
@@ -29,9 +29,12 @@ endef
 define add_host_flags_func
 ifeq ($(filter $(origin $1),undefined default),)
 $(host_arch)_$(host_os)_$1 = $($1)
-$(host_arch)_$(host_os)_$(release_type)_$1 =
 else
 $(host_arch)_$(host_os)_$1 += $($(host_os)_$1)
+endif
+ifneq ($($(1)_CONFIG),)
+$(host_arch)_$(host_os)_$(release_type)_$1 = $($(1)_CONFIG)
+else
 $(host_arch)_$(host_os)_$(release_type)_$1 += $($(host_os)_$(release_type)_$1)
 endif
 host_$1 = $$($(host_arch)_$(host_os)_$1)
