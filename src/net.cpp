13c6a950dd7bbe43dddab49ae11c754781147414
Cory Fields <cory-nospam-@coryfields.com>
2025-04-23 17:16:38 +0000

    net: get rid of confusing and unnecessary nodes_copy temporary
    The other copies have a purpose for locking, this one was just an awkward
    convenience for deleting while iterating.

--- src/net.cpp
+++ src/net.cpp
@@ -1898,14 +1898,11 @@ void CConnman::DisconnectNodes()
         }
 
         // Disconnect unused nodes
-        std::vector<CNode*> nodes_copy = m_nodes;
-        for (CNode* pnode : nodes_copy)
+        for (auto it = m_nodes.begin(); it != m_nodes.end();)
         {
+            const auto& pnode = *it;
             if (pnode->fDisconnect)
             {
-                // remove from m_nodes
-                m_nodes.erase(remove(m_nodes.begin(), m_nodes.end(), pnode), m_nodes.end());
-
                 // Add to reconnection list if appropriate. We don't reconnect right here, because
                 // the creation of a connection is a blocking operation (up to several seconds),
                 // and we don't want to hold up the socket handler thread for that long.
@@ -1933,6 +1930,11 @@ void CConnman::DisconnectNodes()
                 // hold in disconnected pool until all refs are released
                 pnode->Release();
                 nodes_disconnected.push_back(pnode);
+
+                // remove from m_nodes
+                it = m_nodes.erase(it);
+            } else {
+                ++it;
             }
         }
     }
