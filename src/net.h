9413bed2863947d0bf1f6ae426b2c27d8e9f01ed
Cory Fields <cory-nospam-@coryfields.com>
2025-04-23 20:26:00 +0000

    net: don't hold m_nodes_mutex for long-lived callbacks
    The shared_ptrs now ensure that the CNodes haven't been deleted for the
    duration of the callbacks.
    
    Disconnection/Finalization may happen while the shared_ptr is being held,
    though it won't be deleted for the duration of the callback.
    
    The majority of uses are in ProcessMessages and SendMessages, during which
    disconnection may occur but FinalizeNode will not be called.

--- src/net.h
+++ src/net.h
@@ -1151,8 +1151,8 @@ public:
     using NodeFn = std::function<void(CNode*)>;
     void ForEachNode(const NodeFn& func)
     {
-        LOCK(m_nodes_mutex);
-        for (auto&& node : m_nodes) {
+        auto nodes = WITH_LOCK(m_nodes_mutex, return m_nodes);
+        for (auto&& node : nodes) {
             if (NodeFullyConnected(node.get()))
                 func(node.get());
         }
@@ -1160,8 +1160,8 @@ public:
 
     void ForEachNode(const NodeFn& func) const
     {
-        LOCK(m_nodes_mutex);
-        for (auto&& node : m_nodes) {
+        auto nodes = WITH_LOCK(m_nodes_mutex, return m_nodes);
+        for (auto&& node : nodes) {
             if (NodeFullyConnected(node.get()))
                 func(node.get());
         }
