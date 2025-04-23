90f8ae1358bf45edfe63589d749b3a76538e25c8
Cory Fields <cory-nospam-@coryfields.com>
2025-04-23 18:16:44 +0000

    net: finalize nodes on message handler thread
    Rather than relying on refcounting to abstract away our teardown procedure,
    make the rules explicit:
    - FinalizeNode is only called when ProcessMessages/SendMessages is not running.
      This is enforced by finalizing only at the top (or bottom) of the message
      handler loop.
    - FinalizeNode may only be called on nodes which have been disconnected, by
      virtue of finding them in m_nodes_disconnected. This enforces the order:
      disconnect -> finalize -> delete. This order is the current behavior but is
      not strictly necessary, and the restriction will be removed in a later
      commit.
    - Nodes are only deleted after they've been disconnected and finalized. This
      is enforced by deleting them from m_nodes_disconnected and only after
      m_finalized is true.
    - Callers of ForNode/ForEachNode are currently protected from deletion due to
      their locking of m_nodes_mutex. This lock will no longer be necessary after
      CNodes are held as shared_ptrs.

--- src/net.cpp
+++ src/net.cpp
@@ -1964,7 +1964,7 @@ void CConnman::DisconnectNodes()
         // Delete disconnected nodes
         for (auto it = m_nodes_disconnected.begin(); it != m_nodes_disconnected.end();) {
             const auto& pnode = *it;
-            if (pnode->GetRefCount() <= 0) {
+            if (pnode->GetRefCount() <= 0 && pnode->m_finalized) {
                 nodes_to_delete.splice(nodes_to_delete.end(), m_nodes_disconnected, it++);
             } else {
                 ++it;
@@ -1974,7 +1974,6 @@ void CConnman::DisconnectNodes()
     for (const auto& pnode : nodes_to_delete)
     {
         // Destroy the object only after other threads have stopped using it.
-        m_msgproc->FinalizeNode(*pnode);
         DeleteNode(pnode);
     }
     {
@@ -3046,6 +3045,14 @@ void CConnman::ThreadMessageHandler()
         bool fMoreWork = false;
 
         {
+            // Finalize nodes that were marked for deletion on another thread
+            decltype(m_nodes_disconnected) nodes_to_finalize;
+            WITH_LOCK(m_nodes_disconnected_mutex, std::copy_if(m_nodes_disconnected.begin(), m_nodes_disconnected.end(), std::back_inserter(nodes_to_finalize), [](const auto& pnode) { return !pnode->m_finalized; }));
+            for (const auto& pnode : nodes_to_finalize) {
+                m_msgproc->FinalizeNode(*pnode);
+                pnode->m_finalized = true;
+            }
+
             // Randomize the order in which we process messages from/to our peers.
             // This prevents attacks in which an attacker exploits having multiple
             // consecutive connections in the m_nodes list.
@@ -3483,13 +3490,17 @@ void CConnman::StopNodes()
     for (CNode* pnode : nodes) {
         LogDebug(BCLog::NET, "Stopping node, %s", pnode->DisconnectMsg(fLogIPs));
         pnode->CloseSocketDisconnect();
-        m_msgproc->FinalizeNode(*pnode);
+        if (!pnode->m_finalized) {
+            m_msgproc->FinalizeNode(*pnode);
+        }
         DeleteNode(pnode);
     }
     std::list<CNode*> nodes_disconnected;
     WITH_LOCK(m_nodes_disconnected_mutex, nodes_disconnected.swap(m_nodes_disconnected));
     for (CNode* pnode : nodes_disconnected) {
-        m_msgproc->FinalizeNode(*pnode);
+        if (!pnode->m_finalized) {
+            m_msgproc->FinalizeNode(*pnode);
+        }
         DeleteNode(pnode);
     }
     vhListenSocket.clear();
--- src/net.h
+++ src/net.h
@@ -1299,7 +1299,7 @@ private:
     void AddAddrFetch(const std::string& strDest) EXCLUSIVE_LOCKS_REQUIRED(!m_addr_fetches_mutex);
     void ProcessAddrFetch() EXCLUSIVE_LOCKS_REQUIRED(!m_addr_fetches_mutex, !m_unused_i2p_sessions_mutex);
     void ThreadOpenConnections(std::vector<std::string> connect, std::span<const std::string> seed_nodes) EXCLUSIVE_LOCKS_REQUIRED(!m_addr_fetches_mutex, !m_added_nodes_mutex, !m_nodes_mutex, !m_unused_i2p_sessions_mutex, !m_reconnections_mutex);
-    void ThreadMessageHandler() EXCLUSIVE_LOCKS_REQUIRED(!mutexMsgProc);
+    void ThreadMessageHandler() EXCLUSIVE_LOCKS_REQUIRED(!mutexMsgProc, !m_nodes_disconnected_mutex);
     void ThreadI2PAcceptIncoming();
     void AcceptConnection(const ListenSocket& hListenSocket);
 
