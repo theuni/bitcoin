634ac426d5c53b2a09dcea66e25b1de9c0421d54
Cory Fields <cory-nospam-@coryfields.com>
2025-04-28 20:13:08 +0000

    tests: use global connection processing query functions

--- src/test/util/net.h
+++ src/test/util/net.h
@@ -56,7 +56,7 @@ struct ConnmanTestMsg : public CConnman {
         LOCK(m_nodes_mutex);
         m_nodes.push_back(node);
 
-        if (node->IsManualOrFullOutboundConn()) ++m_network_conn_counts[node->addr.GetNetwork()];
+        if (IsManualOrFullOutboundConn(node->m_conn_type)) ++m_network_conn_counts[node->addr.GetNetwork()];
     }
 
     void ClearTestNodes()
