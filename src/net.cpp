a2335d05a0aee2e0214b03f60334b578f35b0bb9
Cory Fields <cory-nospam-@coryfields.com>
2025-04-29 19:43:57 +0000

    net: returning false from MarkReceivedMsgsForProcessing indicates a full queue

--- src/net.cpp
+++ src/net.cpp
@@ -2182,7 +2182,9 @@ void CConnman::SocketHandlerConnected(const std::vector<std::shared_ptr<CNode>>&
                 RecordBytesRecv(nBytes);
                 if (notify) {
                     std::list<CNetMessage> complete_messages = pnode->GetCompleteMessages();
-                    pnode->MarkReceivedMsgsForProcessing(std::move(complete_messages));
+                    if (!pnode->MarkReceivedMsgsForProcessing(std::move(complete_messages))) {
+                        pnode->fPauseRecv = true;
+                    }
                     WakeMessageHandler();
                 }
             }
@@ -3840,7 +3842,7 @@ CNode::CNode(NodeId idIn,
     }
 }
 
-void CNode::MarkReceivedMsgsForProcessing(std::list<CNetMessage> messages)
+bool CNode::MarkReceivedMsgsForProcessing(std::list<CNetMessage> messages)
 {
     AssertLockNotHeld(m_msg_process_queue_mutex);
 
@@ -3854,7 +3856,7 @@ void CNode::MarkReceivedMsgsForProcessing(std::list<CNetMessage> messages)
     LOCK(m_msg_process_queue_mutex);
     m_msg_process_queue.splice(m_msg_process_queue.end(), messages);
     m_msg_process_queue_size += nSizeAdded;
-    fPauseRecv = m_msg_process_queue_size > m_recv_flood_size;
+    return m_msg_process_queue_size <= m_recv_flood_size;
 }
 
 std::optional<std::pair<CNetMessage, bool>> CNode::PollMessage()
