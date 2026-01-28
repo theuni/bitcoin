ec269187a0169d11402919d4a9857c278e8921a2
Cory Fields <cory-nospam-@coryfields.com>
2026-01-28 20:30:18 +0000

    wallet: Modernize NotifyFirstKeyTimeChanged after cleaning up NotifyCanGetAddressesChanged
    Lambdas should be preferred over std::bind.

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -3550,7 +3550,7 @@ void CWallet::ConnectScriptPubKeyManNotifiers()
 {
     for (const auto& spk_man : GetActiveScriptPubKeyMans()) {
         spk_man->NotifyCanGetAddressesChanged.connect([this]{ NotifyCanGetAddressesChanged(); });
-        spk_man->NotifyFirstKeyTimeChanged.connect(std::bind(&CWallet::MaybeUpdateBirthTime, this, std::placeholders::_2));
+        spk_man->NotifyFirstKeyTimeChanged.connect([this](const ScriptPubKeyMan* spkm, int64_t new_birth_time){ MaybeUpdateBirthTime(new_birth_time); });
     }
 }
 
