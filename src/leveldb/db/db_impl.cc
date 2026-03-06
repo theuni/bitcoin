5a9a7fea8ef15b5af650e0d6575d9cdae5e705c9
Cory Fields <cory-nospam-@coryfields.com>
2026-03-06 19:07:14 +0000

    leveldb: avoid dangling-field warning
    Reset the member variable unconditionally.

--- src/leveldb/db/db_impl.cc
+++ src/leveldb/db/db_impl.cc
@@ -625,10 +625,9 @@ void DBImpl::TEST_CompactRange(int level, const Slice* begin,
       background_work_finished_signal_.Wait();
     }
   }
-  if (manual_compaction_ == &manual) {
-    // Cancel my manual compaction since we aborted early for some reason.
-    manual_compaction_ = nullptr;
-  }
+
+  // Reset my manual compaction since in case we aborted early for some reason.
+  manual_compaction_ = nullptr;
 }
 
 Status DBImpl::TEST_CompactMemTable() {
