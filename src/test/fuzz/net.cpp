5d33f3ddc92aea4dd7d829b514850eb170bdb496
Cory Fields <cory-nospam-@coryfields.com>
2026-03-19 18:50:39 +0000

    fuzz: avoid accessing mapLocalHost directly

--- src/test/fuzz/net.cpp
+++ src/test/fuzz/net.cpp
@@ -84,8 +84,7 @@ FUZZ_TARGET(local_address, .init = initialize_net)
     CService service{ConsumeService(fuzzed_data_provider)};
     CNode node{ConsumeNode(fuzzed_data_provider)};
     {
-        LOCK(g_maplocalhost_mutex);
-        mapLocalHost.clear();
+        ClearLocal();
     }
     LIMITED_WHILE(fuzzed_data_provider.ConsumeBool(), 10000) {
         CallOneOf(
