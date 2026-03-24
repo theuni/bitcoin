e1b2fe0f78ca3de99ac6f194dd1800a279c151ed
Cory Fields <cory-nospam-@coryfields.com>
2026-03-24 22:43:51 +0000

    net: move mapLocalHost globals out of net and make them static

--- src/local_addresses.cpp
+++ src/local_addresses.cpp
@@ -7,6 +7,9 @@
 
 #include <net.h>
 
+static GlobalMutex g_maplocalhost_mutex;
+static std::map<CNetAddr, LocalServiceInfo> mapLocalHost GUARDED_BY(g_maplocalhost_mutex);
+
 // Determine the "best" local address for a particular peer.
 [[nodiscard]] std::optional<CService> GetLocalAddress(const CAddress& addr, const Network& network)
 {
--- src/net.h
+++ src/net.h
@@ -158,8 +158,6 @@ extern bool fListen;
 
 /** Subversion as sent to the P2P network in `version` messages */
 extern std::string strSubVersion;
-extern GlobalMutex g_maplocalhost_mutex;
-extern std::map<CNetAddr, LocalServiceInfo> mapLocalHost GUARDED_BY(g_maplocalhost_mutex);
 
 extern const std::string NET_MESSAGE_TYPE_OTHER;
 using mapMsgTypeSize = std::map</* message type */ std::string, /* total bytes */ uint64_t>;
